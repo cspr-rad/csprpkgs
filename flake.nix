696f54d057b2286b57c22bbe2b077d12ad68535c
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-01-22 11:10:09 +0100

    flake: scaffolding

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A collection of casper related packages";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      eachSystem = systems: f:
+        let
+          # Merge together the outputs for all systems.
+          op = attrs: system:
+            let
+              ret = f system;
+              op = attrs: key: attrs //
+                {
+                  ${key} = (attrs.${key} or { })
+                    // { ${system} = ret.${key}; };
+                }
+              ;
+            in
+            builtins.foldl' op attrs (builtins.attrNames ret);
+        in
+        builtins.foldl' op { } systems;
+
+      eachDefaultSystem = eachSystem [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        checks.format = pkgs.runCommand "format-check" { buildInputs = [ pkgs.nixpkgs-fmt ]; } ''
+          set -euo pipefail
+          cd ${self}
+          nixpkgs-fmt --check .
+          touch $out
+        '';
+      });
+}
