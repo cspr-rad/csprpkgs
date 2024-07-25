67a6b9e6cb8bc39b82fad9135b5e54b7ff318cec
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-07-25 12:07:25 +0200

    casper-node: add 2.0

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,6 @@
 final: prev: {
   casper-node = final.callPackage ./pkgs/casper-node { };
+  casper-node_2 = final.callPackage ./pkgs/casper-node/2.nix { };
   casper-node-contracts = final.callPackage ./pkgs/casper-node-contracts { };
   casper-node-launcher = final.callPackage ./pkgs/casper-node-launcher { };
   casper-client-rs = final.callPackage ./pkgs/casper-client-rs { };
